825294741b61685ea84f684d52cd19fa8dbcc5fb
Jose J. Duran <jose-javier.duran@braintec-group.com>
2017-02-15 10:18:42 +0100

    [ADD] stock_connector_yellowcube
    Added picking type record in bindings

--- stock_connector_yellowcube/models/stock_connector_binding_ext.py
+++ stock_connector_yellowcube/models/stock_connector_binding_ext.py
@@ -27,6 +27,11 @@ class StockConnectorBindingExt(models.Model):
                                           compute='get_res_id',
                                           inverse='set_res_id_by_carrier')
 
+    stock_picking_type_id = fields.Many2one('stock.picking.type',
+                                            string='Picking Type',
+                                            compute='get_res_id',
+                                            inverse='set_res_id_by_picktype')
+
     @api.one
     @api.depends('res_id')
     def get_res_id(self):
@@ -34,6 +39,7 @@ class StockConnectorBindingExt(models.Model):
             'product_product_id',
             'stock_location_id',
             'delivery_carrier_id',
+            'stock_picking_type_id',
         ]:
             setattr(self, field, self.res_id)
 
@@ -51,3 +57,8 @@ class StockConnectorBindingExt(models.Model):
     @api.depends('delivery_carrier_id')
     def set_res_id_by_carrier(self):
         self.res_id = self.delivery_carrier_id.id
+
+    @api.one
+    @api.depends('stock_picking_type_id')
+    def set_res_id_by_picktype(self):
+        self.res_id = self.stock_picking_type_id.id
